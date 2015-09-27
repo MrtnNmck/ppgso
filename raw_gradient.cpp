1266860007efcdd783dc9a0937026727e38aea20
MrtnNmck <mrtn.nemcek@gmail.com>
2015-09-28 00:28:16 +0200

    Intended gradient files.

--- raw_gradient.cpp
+++ raw_gradient.cpp
@@ -14,36 +14,40 @@ const unsigned int SIZE = 512;
 // A simple RGB struct will represent a pixel in the framebuffer
 // Note: unsigned char is range <0, 255>
 //       signed char is range <-128, 127>
-struct Pixel {
-  unsigned char r,g,b;
+struct Pixel 
+{
+	unsigned char r, g, b;
 };
 
-int main() {
-  // Initialize a framebuffer
-  Pixel framebuffer[SIZE][SIZE];
+int main()
+{
+	// Initialize a framebuffer
+	Pixel framebuffer[SIZE][SIZE];
 
-  // Example: Generate a simple gradient
-  for ( unsigned int x = 0; x < SIZE; ++x ) {
-    for ( unsigned int y = 0; y < SIZE; ++y ) {
-      framebuffer[x][y].r = x/2;
-      framebuffer[x][y].g = y/2;
-      framebuffer[x][y].b = 0;
-    }
-  }
+	// Example: Generate a simple gradient
+	for (unsigned int x = 0; x < SIZE; ++x)
+	{
+		for (unsigned int y = 0; y < SIZE; ++y)
+		{
+			framebuffer[x][y].r = x / 2;
+			framebuffer[x][y].g = y / 2;
+			framebuffer[x][y].b = 0;
+		}
+	}
 
-  // Task1: Load RAW image file here instead
-  // Task2: Apply a convolution filter to the loaded image
-  // Task3: Merge multiple images using alpha blending
-  // Task4: Draw lines and Bezier curves
+	// Task1: Load RAW image file here instead
+	// Task2: Apply a convolution filter to the loaded image
+	// Task3: Merge multiple images using alpha blending
+	// Task4: Draw lines and Bezier curves
 
-  // Save the raw image to a file
-  std::ofstream raw("result.rgb", std::ios::binary);
-  raw.write((char*)framebuffer, sizeof(framebuffer));
-  raw.close();
-  
-  // Helper object to save the framebuffer as TGA image
-  TGAImage tga(SIZE, SIZE, 3, framebuffer);
-  tga.write_tga_file("result.tga", true);
+	// Save the raw image to a file
+	std::ofstream raw("result.rgb", std::ios::binary);
+	raw.write((char*)framebuffer, sizeof(framebuffer));
+	raw.close();
 
-  return EXIT_SUCCESS;
+	// Helper object to save the framebuffer as TGA image
+	TGAImage tga(SIZE, SIZE, 3, framebuffer);
+	tga.write_tga_file("result.tga", true);
+
+	return EXIT_SUCCESS;
 }
